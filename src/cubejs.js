1e424e1942782ec60b9549df9b1fb70261a13e00
Kevin McLaughlin <github@kevincmclaughlin.com>
2022-05-20 14:37:53 -0500

    feat: v1.0 submitted to data studio

--- src/cubejs.js
+++ src/cubejs.js
@@ -1,45 +1,134 @@
+var TOKEN_EXPIRATION_MINUTES = 10080; // 1 week
+
+function createCubejsUrl(baseUrl, endpoint, parameters) {
+  var url;
+  if (parameters) {
+    url = baseUrl + endpoint + parameters;
+  } else {
+    url = baseUrl + endpoint;
+    Production;
+  }
+  return url;
+}
+
 /**
- * Checks if the path/key provided by the user is valid
- * @param {String} path
+ * Checks if the baseUrl/key provided by the user is valid
+ * @param {String} baseUrl
  * @param {String} key
  * @return {Boolean}
  */
-function checkForValidCreds(path, token) {
+function checkForValidCreds(baseUrl, token, endpoint, parameters) {
+  endpoint = endpoint || "/meta";
+  var url = createCubejsUrl(baseUrl, endpoint, parameters);
+  // need try catch for before user enters auth base url and key
   try {
-    var response = UrlFetchApp.fetch(path + "/meta", {
+    var response = UrlFetchApp.fetch(url, {
       method: "GET",
       headers: {
         Authorization: token,
       },
+      muteHttpExceptions: true,
     });
 
     if (response.getResponseCode() == 200) {
-      return true;
+      return response;
     } else {
-      log(response.getResponseCode());
       resetAuth();
       return false;
     }
   } catch (error) {
-    log(error);
     resetAuth();
     return false;
   }
 }
 
-function setCubejsCredentials(path, key, token, securityContext) {
-  if (!token) {
-    token = generateAccessToken(key, securityContext);
+function getUseCubes(request) {
+  var useCubes;
+  if (request.configParams.useCubes) {
+    useCubes =
+      request.configParams.securityContext === "[]"
+        ? "All"
+        : JSON.parse(request.configParams.useCubes);
   }
-  var validCreds = checkForValidCreds(path, token);
+  return useCubes;
+}
+
+function getSecurityContext(request) {
+  var securityContext;
+  if (request.configParams.securityContext) {
+    securityContext =
+      request.configParams.securityContext === "{}"
+        ? undefined
+        : JSON.parse(request.configParams.securityContext);
+  }
+  return securityContext;
+}
+
+function setCubejsCredentials(
+  baseUrl,
+  key,
+  token,
+  securityContext,
+  endpoint,
+  parameter
+) {
+  if (!token) {
+    token = generateAccessToken(key, securityContext, TOKEN_EXPIRATION_MINUTES);
+  }
+  var validCreds = checkForValidCreds(baseUrl, token, endpoint, parameter);
   if (!validCreds) {
     return validCreds;
   }
   var userProperties = PropertiesService.getUserProperties();
-  userProperties.setProperty("dscc.path", path);
+  userProperties.setProperty("dscc.baseUrl", baseUrl);
   userProperties.setProperty("dscc.key", key);
   userProperties.setProperty("dscc.token", token);
   return validCreds;
+}
+
+function cubejectApiReqest(
+  baseUrl,
+  token,
+  endpoint,
+  parameters,
+  securityContext
+) {
+  var url = createCubejsUrl(baseUrl, endpoint, parameters);
+  var response = UrlFetchApp.fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+    muteHttpExceptions: true,
+  });
+  var responseCode = response.getResponseCode();
+  var responseBody = response.getContentText();
+  if (responseCode === 403) {
+    // if response is 403, try reauth token one time
+    var userProperties = PropertiesService.getUserProperties();
+    var key = userProperties.getProperty("dscc.key");
+    response = setCubejsCredentials(
+      baseUrl,
+      key,
+      null,
+      securityContext,
+      endpoint,
+      parameters
+    );
+    if (response) {
+      responseCode = response.getResponseCode();
+      responseBody = response.getContentText();
+    } else {
+      throw new Error(
+        "Check your cubejs credentials. Could not generate a valid token."
+      );
+    }
+  }
+  if (responseCode === 200) {
+    return JSON.parse(responseBody);
+  } else {
+    throw new Error(responseCode + ": " + responseBody);
+  }
 }
 
 function cubejsTypesToDataStudioTypes(cubejsField) {
@@ -97,20 +186,4 @@ function cubejsTypesToDataStudioTypes(cubejsField) {
     },
   };
   return types[cubejsField.type](cubejsField);
-}
-
-function cubejectApiReqest(url, token, path, parameters) {
-  var endpoint;
-  if (parameters) {
-    endpoint = url + path + parameters;
-  } else {
-    endpoint = url + path;
-  }
-  var response = UrlFetchApp.fetch(endpoint, {
-    method: "GET",
-    headers: {
-      Authorization: token,
-    },
-  });
-  return JSON.parse(response.getContentText());
 }
